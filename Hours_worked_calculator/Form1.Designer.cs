78c6d627c3e2dd0ab38635d2838c13110cdc19dc
User <User@DESKTOP-JSSAPJE>
2022-10-23 21:18:09 -0600

    Agregar archivos de proyecto.

---
+++ Hours_worked_calculator/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace Hours_worked_calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkson = new System.Windows.Forms.CheckBox();
+            this.textday = new System.Windows.Forms.TextBox();
+            this.textdaysalary = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textnetsalary = new System.Windows.Forms.TextBox();
+            this.textbonus = new System.Windows.Forms.TextBox();
+            this.textgrosssalary = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(94, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Days Worked:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Pay per Work Day:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(135, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Gross Salary:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 73);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Bonus:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.textdaysalary);
+            this.groupBox1.Controls.Add(this.textday);
+            this.groupBox1.Controls.Add(this.checkson);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(383, 210);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Data";
+            // 
+            // checkson
+            // 
+            this.checkson.AutoSize = true;
+            this.checkson.Location = new System.Drawing.Point(138, 108);
+            this.checkson.Name = "checkson";
+            this.checkson.Size = new System.Drawing.Size(130, 17);
+            this.checkson.TabIndex = 5;
+            this.checkson.Text = "Do you Have Children";
+            this.checkson.UseVisualStyleBackColor = true;
+            this.checkson.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // textday
+            // 
+            this.textday.Location = new System.Drawing.Point(138, 30);
+            this.textday.Name = "textday";
+            this.textday.Size = new System.Drawing.Size(189, 20);
+            this.textday.TabIndex = 6;
+            // 
+            // textdaysalary
+            // 
+            this.textdaysalary.Location = new System.Drawing.Point(138, 66);
+            this.textdaysalary.Name = "textdaysalary";
+            this.textdaysalary.Size = new System.Drawing.Size(189, 20);
+            this.textdaysalary.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(211, 136);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 37);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Clean";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Location = new System.Drawing.Point(12, 228);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(383, 210);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Developer";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textgrosssalary);
+            this.groupBox3.Controls.Add(this.textbonus);
+            this.groupBox3.Controls.Add(this.textnetsalary);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Location = new System.Drawing.Point(405, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(383, 210);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Result";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 111);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Net Salary:";
+            // 
+            // textnetsalary
+            // 
+            this.textnetsalary.Location = new System.Drawing.Point(100, 108);
+            this.textnetsalary.Name = "textnetsalary";
+            this.textnetsalary.Size = new System.Drawing.Size(189, 20);
+            this.textnetsalary.TabIndex = 8;
+            // 
+            // textbonus
+            // 
+            this.textbonus.Location = new System.Drawing.Point(100, 72);
+            this.textbonus.Name = "textbonus";
+            this.textbonus.Size = new System.Drawing.Size(189, 20);
+            this.textbonus.TabIndex = 9;
+            // 
+            // textgrosssalary
+            // 
+            this.textgrosssalary.Location = new System.Drawing.Point(100, 35);
+            this.textgrosssalary.Name = "textgrosssalary";
+            this.textgrosssalary.Size = new System.Drawing.Size(189, 20);
+            this.textgrosssalary.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(74, 83);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 13);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "MI06B";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(29, 83);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(39, 13);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Group:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(73, 53);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(145, 13);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "Carlos Enrique Ulloa Martinez";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(29, 53);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(38, 13);
+            this.label10.TabIndex = 5;
+            this.label10.Text = "Name:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textdaysalary;
+        private System.Windows.Forms.TextBox textday;
+        private System.Windows.Forms.CheckBox checkson;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textgrosssalary;
+        private System.Windows.Forms.TextBox textbonus;
+        private System.Windows.Forms.TextBox textnetsalary;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Hours_worked_calculator/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hours_worked_calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //we declare variables
+            int dias, pagos, salario_bruto;
+            double bonificacion, salario_neto;
+            bool hijos;
+            //read data
+            dias = Convert.ToInt32(textday.Text);
+            pagos = Convert.ToInt32(textdaysalary.Text);
+            hijos = Convert.ToBoolean(checkson.Checked);
+
+            salario_bruto = dias * pagos;
+
+            //condictionals
+            if(salario_bruto < 1000) {
+
+                bonificacion = Convert.ToDouble(salario_bruto * 0.1);
+            }
+            else { 
+            
+                if(salario_bruto >= 1000 && salario_bruto <= 2500) {
+
+                    bonificacion = Convert.ToDouble(salario_bruto * 0.2);
+                }
+                else {
+
+                    bonificacion = Convert.ToDouble(salario_bruto * 0.25);
+                }
+            }
+
+            //second condictional
+            if (hijos == true) {
+
+                bonificacion = bonificacion * 2;
+            }
+
+            salario_neto = Convert.ToDouble(salario_bruto + bonificacion);
+
+            textgrosssalary.Text = Convert.ToString(salario_bruto);
+            textbonus.Text = Convert.ToString(bonificacion);
+            textnetsalary.Text = Convert.ToString(salario_neto);
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //clear
+            textdaysalary.Clear();
+            textday.Clear();
+            textgrosssalary.Clear();
+            textbonus.Clear();
+            textnetsalary.Clear();
+
+            //focus
+            textday.Focus();
+        }
+    }
+}
